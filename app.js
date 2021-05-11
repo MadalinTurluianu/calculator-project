14a7715b2ebfdae1355b89a0c41b622e150ea55b
Madalin Turluianu <turluianu.petre.madalin@gmail.com>
2021-05-11 22:23:13 +0300

    work in progress

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-const digit_0 = document.querySelector(".digit-0");
-const digit_1 = document.querySelector(".digit-1");
-const digit_2 = document.querySelector(".digit-2");
-const digit_3 = document.querySelector(".digit-3");
-const digit_4 = document.querySelector(".digit-4");
-const digit_5 = document.querySelector(".digit-5");
-const digit_6 = document.querySelector(".digit-6");
-const digit_7 = document.querySelector(".digit-7");
-const digit_8 = document.querySelector(".digit-8");
-const digit_9 = document.querySelector(".digit-9");
-const digit_add = document.querySelector(".digit-add");
-const digit_subtract = document.querySelector(".digit-subtract");
-const digit_multiply = document.querySelector(".digit-multiply");
-const digit_divide = document.querySelector(".digit-divide");
-const digit_equal = document.querySelector(".digit-equal");
-const digit_back = document.querySelector(".digit-back");
-const digit_cancel = document.querySelector(".digit-cancel");
-const digit_dot = document.querySelector(".digit-dot");
-
 // --------------------------------------------- OPORATIONS FUNCTIONS
 
 function add(total,...numbers){
@@ -64,7 +45,63 @@ function operate(operator, total, userInput = []){
         result = subtract(total, ...userInput);
     }else if (operator === "*") {
         result = multiply(total, ...userInput);
-    }else {
+    }else if (operator === "/"){
         result = divide(total, ...userInput);
     }
+    return result;
 }
+
+// --------------------------------------------- ADD NUMBERS ON THE DISPLAY
+
+const numbersBtns = Array.from(document.querySelectorAll(".nr"));
+const screen = document.querySelector(".screen h2");
+let nrBeforeOp = []
+let display = [];
+
+
+for (btn of numbersBtns) {
+    btn.addEventListener("click",function(){
+        display.push(this.textContent);
+        screen.textContent = display.join("");
+    })
+}
+
+// --------------------------------------------- SET OPERATOR
+
+const operatorsBtns = Array.from(document.querySelectorAll(".op"));
+let operator;
+
+for (btn of operatorsBtns) {
+    btn.addEventListener("click", function(){
+        nrBeforeOp = [...display];
+        operator = this.textContent;
+        display = [];
+    });
+}
+
+// --------------------------------------------- DO THE OPERATION
+
+const equalBtn = document.querySelector(".digit-equal");
+
+equalBtn.addEventListener("click", function(){
+    display = [operate(operator, parseInt(nrBeforeOp.join("")), [parseInt(display.join(""))])];
+    screen.textContent = display.join("");
+})
+
+// --------------------------------------------- CANCEL FUNCTION
+
+const cancelBtn = document.querySelector(".digit-cancel");
+
+cancelBtn.addEventListener("click", function(){
+    display = [];
+    screen.textContent = "";
+})
+
+// --------------------------------------------- BACK FUNCTION
+
+const backBtn = document.querySelector(".digit-back");
+
+backBtn.addEventListener("click", function(){
+    display.pop()
+    screen.textContent = display.join("");
+})
